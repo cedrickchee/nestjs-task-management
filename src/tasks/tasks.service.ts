e3413ef9aa633ae56c01a0bc28bd24e82b679b2c
Cedric Chee <cedric@invictusbyte.com>
2021-06-13 12:29:19 +0800

    refactor: before persistence of getting task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,28 +13,9 @@ export class TasksService {
     private taskRepository: TaskRepository,
   ) {}
 
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
-
-  // getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-
-  //   let tasks = this.getAllTasks();
-
-  //   if (status) {
-  //     tasks = tasks.filter((task) => task.status === status);
-  //   }
-
-  //   if (search) {
-  //     tasks = tasks.filter(
-  //       (task) =>
-  //         task.title.includes(search) || task.description.includes(search),
-  //     );
-  //   }
-
-  //   return tasks;
-  // }
+  getTasks(filterDto: GetTasksFilterDto) {
+    //
+  }
 
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
